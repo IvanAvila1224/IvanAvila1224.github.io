e4e21512b752bb6eeb37e46df93672dfdb88f9b6
Ivan1224 <magis@Ivan.com>
2022-09-11 13:21:36 -0500

    create branch skills #9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HearderComponent } from './hearder/hearder.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
 import { EducationAndCompetencesComponent } from './education-and-competences/education-and-competences.component';
+import { SkillsAndCompetencesComponent } from './skills-and-competences/skills-and-competences.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HearderComponent,
     WorkExperienceComponent,
-    EducationAndCompetencesComponent
+    EducationAndCompetencesComponent,
+    SkillsAndCompetencesComponent
   ],
   imports: [
     BrowserModule
