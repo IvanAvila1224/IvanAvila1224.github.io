ec35648228565b8de84b5f98edd3b407d4bd5466
Ivan1224 <magis@Ivan.com>
2022-09-12 00:13:13 -0500

    nueva congiuracion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,7 @@ import { AppComponent } from './app.component';
 import { HearderComponent } from './hearder/hearder.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
 import { EducationAndCompetencesComponent } from './education-and-competences/education-and-competences.component';
-<<<<<<< HEAD
 import { CertificatesComponent } from './certificates/certificates.component';
-=======
-import { SkillsAndCompetencesComponent } from './skills-and-competences/skills-and-competences.component';
->>>>>>> 0b64eebb8e7e86e2bb95e581e566eff6ffc1015a
 
 @NgModule({
   declarations: [
@@ -17,11 +13,7 @@ import { SkillsAndCompetencesComponent } from './skills-and-competences/skills-a
     HearderComponent,
     WorkExperienceComponent,
     EducationAndCompetencesComponent,
-<<<<<<< HEAD
     CertificatesComponent
-=======
-    SkillsAndCompetencesComponent
->>>>>>> 0b64eebb8e7e86e2bb95e581e566eff6ffc1015a
   ],
   imports: [
     BrowserModule
