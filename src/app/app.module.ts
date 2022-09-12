955c00ec422cb5f3eb5a3e281c5fe3455fe8a681
Ivan1224 <magis@Ivan.com>
2022-09-12 00:25:42 -0500

    import skills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HearderComponent } from './hearder/hearder.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
 import { EducationAndCompetencesComponent } from './education-and-competences/education-and-competences.component';
 import { CertificatesComponent } from './certificates/certificates.component';
-
+import { SkillsAndCompetencesComponent } from './skills-and-competences/skills-and-competences.component';
 @NgModule({
   declarations: [
     AppComponent,
